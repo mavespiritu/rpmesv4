3d8ee6cda112990115b4c2c93764d3db3e82088a
Mark Anthony Espiritu <mark.a.v.espiritu@gmail.com>
2022-02-24 13:43:24 +0800

    created prexc in ris table

--- common/modules/v1/models/Ris.php
+++ common/modules/v1/models/Ris.php
@@ -311,11 +311,13 @@ class Ris extends \yii\db\ActiveRecord
                         ppmp_sub_program.code,"",
                         ppmp_identifier.code,"",
                         ppmp_pap.code,"000-",
-                        ppmp_activity.code
+                        ppmp_activity.code,"-",
+                        ppmp_sub_activity.code
                     ) as prexc'
                 ])
                 ->leftJoin('ppmp_ppmp_item', 'ppmp_ppmp_item.id = ppmp_ris_item.ppmp_item_id')
                 ->leftJoin('ppmp_activity', 'ppmp_activity.id = ppmp_ppmp_item.activity_id')
+                ->leftJoin('ppmp_sub_activity', 'ppmp_sub_activity.id = ppmp_ppmp_item.sub_activity_id')
                 ->leftJoin('ppmp_pap', 'ppmp_pap.id = ppmp_activity.pap_id')
                 ->leftJoin('ppmp_identifier', 'ppmp_identifier.id = ppmp_pap.identifier_id')
                 ->leftJoin('ppmp_sub_program', 'ppmp_sub_program.id = ppmp_pap.sub_program_id')
@@ -343,11 +345,13 @@ class Ris extends \yii\db\ActiveRecord
                         ppmp_sub_program.code,"",
                         ppmp_identifier.code,"",
                         ppmp_pap.code,"000-",
-                        ppmp_activity.code
+                        ppmp_activity.code,"-",
+                        ppmp_sub_activity.code
                     ) as prexc'
                 ])
                 ->leftJoin('ppmp_ppmp_item', 'ppmp_ppmp_item.id = ppmp_ris_item.ppmp_item_id')
                 ->leftJoin('ppmp_activity', 'ppmp_activity.id = ppmp_ppmp_item.activity_id')
+                ->leftJoin('ppmp_sub_activity', 'ppmp_sub_activity.id = ppmp_ppmp_item.sub_activity_id')
                 ->leftJoin('ppmp_pap', 'ppmp_pap.id = ppmp_activity.pap_id')
                 ->leftJoin('ppmp_identifier', 'ppmp_identifier.id = ppmp_pap.identifier_id')
                 ->leftJoin('ppmp_sub_program', 'ppmp_sub_program.id = ppmp_pap.sub_program_id')
