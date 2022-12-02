7ce681d1b503f424e68a45e5c44681a0f1ede177
unknown <andrew.seromlav@gmail.com>
2022-12-02 17:16:19 +0800

    dashboard: employment graph

---
+++ frontend/views/site/_dashboard-filter.php
@@ -0,0 +1,162 @@
+<?php
+
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\widgets\ActiveForm;
+use kartik\select2\Select2;
+use yii\web\View;
+use faryshta\disableSubmitButtons\Asset as DisableButtonAsset;
+DisableButtonAsset::register($this);
+/* @var $this yii\web\View */
+/* @var $model common\modules\rpmes\models\DueDateSearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+<style>
+#filterForm {
+  display : none;
+}
+</style>
+<div id="filterForm">
+<hr>
+<?php $form = ActiveForm::begin([
+    'id' => 'search-dashboard-form'
+]); ?>
+    <div class="row">
+        <div class="col-md-3 col-xs-12">
+            <?= $form->field($model, 'year')->widget(Select2::classname(), [
+                'data' => $years,
+                'options' => ['multiple' => false, 'placeholder' => 'Select One', 'class'=>'year-select'],
+                'pluginOptions' => [
+                    'allowClear' =>  true,
+                ],
+                ])->label('Year *');
+            ?>
+        </div>
+        <div class="col-md-3 col-xs-12">
+            <?= $form->field($model, 'quarter')->widget(Select2::classname(), [
+                'data' => $quarters,
+                'options' => ['multiple' => false, 'placeholder' => 'Select One', 'class'=>'quarter-select'],
+                'pluginOptions' => [
+                    'allowClear' =>  true,
+                ],
+                ])->label('Quarter *');
+            ?>
+        </div>
+        <div class="col-md-3 col-xs-12">
+            <?= $form->field($model, 'agency_id')->widget(Select2::classname(), [
+                'data' => $agencies,
+                'options' => ['multiple' => false, 'placeholder' => 'Select One', 'class'=>'agency-select'],
+                'pluginOptions' => [
+                    'allowClear' =>  true,
+                ],
+                ])->label('Agency');
+            ?>
+        </div>
+        <div class="col-md-3 col-xs-12">
+            <?= $form->field($model, 'category_id')->widget(Select2::classname(), [
+                'data' => $categories,
+                'options' => ['multiple' => false, 'placeholder' => 'Select one', 'class'=>'category-select'],
+                'pluginOptions' => [
+                    'allowClear' =>  true,
+                ],
+                ]);
+            ?>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-3 col-xs-12">
+            <?= $form->field($model, 'sector_id')->widget(Select2::classname(), [
+                    'data' => $sectors,
+                    'options' => ['multiple' => false, 'placeholder' => 'Select one', 'class'=>'sector-select'],
+                    'pluginOptions' => [
+                        'allowClear' =>  true,
+                    ],
+                    'pluginEvents'=>[
+                        'select2:select'=>'
+                            function(){
+                                $.ajax({
+                                    url: "'.Url::to(['/rpmes/project/sub-sector-list']).'",
+                                    data: {
+                                            id: this.value,
+                                        }
+                                }).done(function(result) {
+                                    $(".sub-sector-select").html("").select2({ data:result, theme:"krajee", width:"100%",placeholder:"Select one", allowClear: true});
+                                    $(".sub-sector-select").select2("val","");
+                                });
+                            }'
+                        
+                    ]
+                    ]);
+                ?>
+        </div>
+        <div class="col-md-3 col-xs-12">
+                <?= $form->field($model, 'sub_sector_id')->widget(Select2::classname(), [
+                    'data' => $subSectors,
+                    'options' => ['multiple' => false, 'placeholder' => 'Select one', 'class'=>'sub-sector-select'],
+                    'pluginOptions' => [
+                        'allowClear' =>  true,
+                    ],
+                    ]);
+                ?>
+        </div>
+        <div class="col-md-3 col-xs-12">
+            <?= $form->field($model, 'province_id')->widget(Select2::classname(), [
+                'data' => $provinces,
+                'options' => ['multiple' => false, 'placeholder' => 'Select one', 'class'=>'province-select'],
+                'pluginOptions' => [
+                    'allowClear' =>  true,
+                ],
+                ]);
+            ?>
+        </div>
+        <div class="col-md-3 col-xs-12">
+            <?= $form->field($model, 'fund_source_id')->widget(Select2::classname(), [
+                'data' => $fundSources,
+                'options' => ['multiple' => false, 'placeholder' => 'Select One', 'class'=>'fund-source-select'],
+                'pluginOptions' => [
+                    'allowClear' =>  true,
+                ],
+                ])->label('Fund Source');
+            ?>
+        </div>
+    </div>
+    <div class="form-group pull-right">
+        <?= Html::submitButton('Generate Data', ['class' => 'btn btn-primary', 'style' => 'margin-top: 5px;']) ?>
+    </div>
+    <div class="clearfix"></div>
+</div>
+<hr>
+<?php ActiveForm::end(); ?>
+<?php
+    $script = '
+    $("#search-summary-summary-accomplishment-form").on("beforeSubmit", function (e) {
+        e.preventDefault();
+     
+        var form = $(this);
+        var formData = form.serialize();
+        
+        $.ajax({
+            url: form.attr("action"),
+            type: form.attr("method"),
+            data: formData,
+            beforeSend: function(){
+                $("#summary-summary-accomplishment-table").html("<div class=\"text-center\"><svg class=\"spinner\" width=\"30px\" height=\"30px\" viewBox=\"0 0 66 66\" xmlns=\"http://www.w3.org/2000/svg\"><circle class=\"path\" fill=\"none\" stroke-width=\"6\" stroke-linecap=\"round\" cx=\"33\" cy=\"33\" r=\"30\"></circle></svg></div>");
+            },
+            success: function (data) {
+                console.log(this.data);
+                $("#summary-summary-accomplishment-table").empty();
+                $("#summary-summary-accomplishment-table").hide();
+                $("#summary-summary-accomplishment-table").fadeIn("slow");
+                $("#summary-summary-accomplishment-table").html(data);
+            },
+            error: function (err) {
+                console.log(err);
+            }
+        });      
+
+        return false;
+    });
+    ';
+
+    $this->registerJs($script, View::POS_END);
+?>
--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -22,118 +22,17 @@ $this->title = 'eRPMES';
 
     <div class='body-content container'>
     <button onclick="showHideFilter()" class='btn btn-lg btn-primary' align=right>&#8595; Filter</button>
-<div id="filterForm">
-<hr>
-<?php $form = ActiveForm::begin([
-    'id' => 'search-dashboard-form'
-]); ?>
-    <div class="row">
-        <div class="col-md-3 col-xs-12">
-            <?= $form->field($model, 'year')->widget(Select2::classname(), [
-                'data' => $years,
-                'options' => ['multiple' => false, 'placeholder' => 'Select One', 'class'=>'year-select'],
-                'pluginOptions' => [
-                    'allowClear' =>  true,
-                ],
-                ])->label('Year *');
-            ?>
-        </div>
-        <div class="col-md-3 col-xs-12">
-            <?= $form->field($model, 'quarter')->widget(Select2::classname(), [
-                'data' => $quarters,
-                'options' => ['multiple' => false, 'placeholder' => 'Select One', 'class'=>'quarter-select'],
-                'pluginOptions' => [
-                    'allowClear' =>  true,
-                ],
-                ])->label('Quarter *');
-            ?>
-        </div>
-        <div class="col-md-3 col-xs-12">
-            <?= $form->field($model, 'agency_id')->widget(Select2::classname(), [
-                'data' => $agencies,
-                'options' => ['multiple' => false, 'placeholder' => 'Select One', 'class'=>'agency-select'],
-                'pluginOptions' => [
-                    'allowClear' =>  true,
-                ],
-                ])->label('Agency');
-            ?>
-        </div>
-        <div class="col-md-3 col-xs-12">
-            <?= $form->field($model, 'category_id')->widget(Select2::classname(), [
-                'data' => $categories,
-                'options' => ['multiple' => false, 'placeholder' => 'Select one', 'class'=>'category-select'],
-                'pluginOptions' => [
-                    'allowClear' =>  true,
-                ],
-                ]);
-            ?>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-3 col-xs-12">
-            <?= $form->field($model, 'sector_id')->widget(Select2::classname(), [
-                    'data' => $sectors,
-                    'options' => ['multiple' => false, 'placeholder' => 'Select one', 'class'=>'sector-select'],
-                    'pluginOptions' => [
-                        'allowClear' =>  true,
-                    ],
-                    'pluginEvents'=>[
-                        'select2:select'=>'
-                            function(){
-                                $.ajax({
-                                    url: "'.Url::to(['/rpmes/project/sub-sector-list']).'",
-                                    data: {
-                                            id: this.value,
-                                        }
-                                }).done(function(result) {
-                                    $(".sub-sector-select").html("").select2({ data:result, theme:"krajee", width:"100%",placeholder:"Select one", allowClear: true});
-                                    $(".sub-sector-select").select2("val","");
-                                });
-                            }'
-                        
-                    ]
-                    ]);
-                ?>
-        </div>
-        <div class="col-md-3 col-xs-12">
-                <?= $form->field($model, 'sub_sector_id')->widget(Select2::classname(), [
-                    'data' => $subSectors,
-                    'options' => ['multiple' => false, 'placeholder' => 'Select one', 'class'=>'sub-sector-select'],
-                    'pluginOptions' => [
-                        'allowClear' =>  true,
-                    ],
-                    ]);
-                ?>
-        </div>
-        <div class="col-md-3 col-xs-12">
-            <?= $form->field($model, 'province_id')->widget(Select2::classname(), [
-                'data' => $provinces,
-                'options' => ['multiple' => false, 'placeholder' => 'Select one', 'class'=>'province-select'],
-                'pluginOptions' => [
-                    'allowClear' =>  true,
-                ],
-                ]);
-            ?>
-        </div>
-        <div class="col-md-3 col-xs-12">
-            <?= $form->field($model, 'fund_source_id')->widget(Select2::classname(), [
-                'data' => $fundSources,
-                'options' => ['multiple' => false, 'placeholder' => 'Select One', 'class'=>'fund-source-select'],
-                'pluginOptions' => [
-                    'allowClear' =>  true,
-                ],
-                ])->label('Fund Source');
-            ?>
-        </div>
-    </div>
-    <div class="form-group pull-right">
-        <?= Html::submitButton('Generate Data', ['class' => 'btn btn-primary', 'style' => 'margin-top: 5px;']) ?>
-    </div>
-    <div class="clearfix"></div>
-</div>
-
-<hr>
-<?php ActiveForm::end(); ?>
+        <?= $this->render('_dashboard-filter', [
+                        'model' => $model,
+                        'years' => $years,
+                        'quarters' => $quarters,
+                        'agencies' => $agencies,
+                        'sectors' => $sectors,
+                        'subSectors' => $subSectors,
+                        'categories' => $categories,
+                        'provinces' => $provinces,
+                        'fundSources' => $fundSources,
+                    ]) ?>
 
         <div class='row'>
             <div class='col-lg-6'>
@@ -147,6 +46,12 @@ $this->title = 'eRPMES';
                 <h3 style='color: #3C8DBC;'><i class='fa fa-area-chart'></i>Statistics</h3>
 
                 <div id="graphs-table">
+                <?= $this->render('_graphs', [
+                        'projectStatus' => $projectStatus,
+                        'projectFinancial' => $projectFinancial,
+                        'scriptEmployment' => $scriptEmployment,
+                        'script' => $script,
+                    ]) ?>
 
             </div>
         </div>
